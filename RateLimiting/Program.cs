07563f563a0954497272154b783a937859b01dd1
Damian Edwards <damian@damianedwards.com>
2022-10-12 14:21:35 -0700

    Update Program.cs

--- RateLimiting/Program.cs
+++ RateLimiting/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddRateLimiter(options =>
 {
     options.OnRejected = OnRateLimitRejected;
