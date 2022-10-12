e61dd7ee87770d1db031150e715fe3cd9f31b08a
David Fowler <davidfowl@gmail.com>
2022-10-12 09:08:07 -0700

    Added a better example.

--- PerRouteMiddleware/Program.cs
+++ PerRouteMiddleware/Program.cs
@@ -1,9 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!").Use(sub =>
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/foo", (string q) => q).Use(sub =>
 {
-    sub.UseWelcomePage();
+    sub.Use((context, next) =>
+    {
+        context.Request.QueryString = QueryString.Create("q", "this is a query string value");
+
+        return next(context);
+    });
+});
+
+app.MapGet("/bar", ([FromHeader(Name = "X-Path")]string s) => s).Use(sub =>
+{
+    sub.Use((context, next) =>
+    {
+        context.Request.Headers["X-Path"] = "this is a header value";
+
+        return next(context);
+    });
 });
 
 app.Run();
