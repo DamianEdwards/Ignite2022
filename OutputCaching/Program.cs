65a3db35c41e1cda3b9bfe57a3f7601bfc41d00d
Damian Edwards <damian@damianedwards.com>
2022-10-12 12:09:31 -0700

    Update caching demo

--- OutputCaching/Program.cs
+++ OutputCaching/Program.cs
@@ -1,33 +1,32 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOutputCache();
-
 var app = builder.Build();
 
-app.UseResponseCaching();
-app.UseOutputCache();
-
-app.MapGet("/", (HttpContext context) =>
+app.MapGet("/", (int? size, HttpContext context) =>
     {
         const string type = "monsterid";
-        const int size = 200;
+        size ??= 200;
         var hash = Guid.NewGuid().ToString("n");
-
-        context.Response.GetTypedHeaders().CacheControl = new()
-        {
-            Public = true,
-            MaxAge = TimeSpan.FromSeconds(5)
-        };
 
         var html = $"""
                     <img src="https://www.gravatar.com/avatar/{hash}?s={size}&d={type}" width="{size}" height="{size}" />
                     <pre>Generated at {DateTime.Now:O}</pre>
-                    <a href="/">Load</a>
+                    <a href="/?size={size}">Load</a>
                     """;
 
         return Results.Text(html, "text/html");
-    })
-    //.CacheOutput()
-    ;
+    });
 
 app.Run();
+
+#region codez
+//if (context.Features.Get<IResponseCachingFeature>() is { } responseCaching)
+//{
+//    responseCaching.VaryByQueryKeys = new[] { "size" };
+//    context.Response.GetTypedHeaders().CacheControl = new()
+//    {
+//        Public = true,
+//        MaxAge = TimeSpan.FromSeconds(5)
+//    };
+//}
+#endregion
