5559ef8fb35644e5bef82f04605ac67609a4d826
David Fowler <davidfowl@gmail.com>
2022-10-11 19:57:05 -0700

    Add w3c logs

--- RequestLogging/Program.cs
+++ RequestLogging/Program.cs
@@ -8,9 +8,15 @@ builder.Services.AddHttpLogging(o =>
     o.LoggingFields = HttpLoggingFields.All;
 });
 
+builder.Services.AddW3CLogging(o =>
+{
+    o.LoggingFields = W3CLoggingFields.All;
+});
+
 var app = builder.Build();
 
 app.UseHttpLogging();
+app.UseW3CLogging();
 
 app.MapGet("/", () => "Hello World!");
 app.MapPost("/", (JsonObject obj) => obj);
